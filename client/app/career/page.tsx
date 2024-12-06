974d43fc9b84bddd53a778e593955beb13175bab
Abhijith Babu <abijithb2002@gmail.com>
2024-12-07 01:58:31 +0530

    hotfix: career-page suspense boundry build error

--- client/app/career/page.tsx
+++ client/app/career/page.tsx
@@ -1,22 +1,25 @@
-import CustomJob from '@/components/career/CustomJob'
-import HeroSection from '@/components/career/HeroSection'
-import JoinOurTeam from '@/components/career/JoinOurTeam'
-import { Metadata } from 'next';
-import React from 'react'
+import CustomJob from "@/components/career/CustomJob";
+import HeroSection from "@/components/career/HeroSection";
+import JoinOurTeam from "@/components/career/JoinOurTeam";
+import { Metadata } from "next";
+import React, { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Career | Infinite Group",
-  description: "Apply today & connect with your future! @ Infinite Group of Companies",
+  description:
+    "Apply today & connect with your future! @ Infinite Group of Companies",
 };
 
 const page = () => {
   return (
     <main>
-      <HeroSection/>
-      <JoinOurTeam/>
-      <CustomJob/>
+      <Suspense>
+        <HeroSection />
+        <JoinOurTeam />
+        <CustomJob />
+      </Suspense>
     </main>
-  )
-}
+  );
+};
 
-export default page
+export default page;
