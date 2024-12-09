6d20117aa7772f93c42a25f463d0811268096c78
Asherprajan <asherprajan@gmail.com>
2024-12-10 04:48:30 +0530

    removed the banner

--- client/components/home/Landing.tsx
+++ client/components/home/Landing.tsx
@@ -12,12 +12,12 @@ const Landing: FC = () => {
         muted
         playsInline
       />
-      <div className="absolute bg-opacity-60 z-1 bg-black w-full h-full" />
-      <div className="absolute bg-opacity-40 z-1 bg-[#16233F] rounded-full w-[140vw] h-[140vw] lg:h-[120vh] lg:w-[120vh]"></div>
-      <div className="absolute bg-opacity-40 z-1 bg-[#16233F] rounded-full w-[100vw] h-[100vw] lg:h-[90vh] lg:w-[90vh]"></div>
+      <div className="absolute bg-opacity-10 z-1  w-full h-full" />
+      {/* <div className="absolute bg-opacity-40 z-1 bg-[#16233F] rounded-full w-[140vw] h-[140vw] lg:h-[120vh] lg:w-[120vh]"></div>
+      <div className="absolute bg-opacity-40 z-1 bg-[#16233F] rounded-full w-[100vw] h-[100vw] lg:h-[90vh] lg:w-[90vh]"></div> */}
       <div className="flex flex-col gap-8 row-start-2 items-center z-10">
         <h1 className="text-3xl md:text-5xl lg:text-7xl font-bold text-center text-opacity-80 font-plus_jakarta leading-snug md:leading-tight pb-8 tracking-wider max-w-6xl">
-          Innovating Solutions Across Multiple Industries
+          {/* Innovating Solutions Across Multiple Industries */}
         </h1>
         <CustomButton type="primary">Get Started Now</CustomButton>
       </div>
